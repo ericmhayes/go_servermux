67789e47ab817cbbe77fa0cf9357fd61389fca3e
Eric Hayes <38408059+ericmhayes@users.noreply.github.com>
2022-12-30 12:42:38 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type timeHandler struct {
+	format string
+}
+
+func (th timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(th.format)
+	w.Write([]byte("The time is: " + tm))
+}
+
+func timeHandlerRefactored(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(time.RFC1123)
+	w.Write([]byte("The time is: " + tm))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to create an empty servemux.
+	mux := http.NewServeMux()
+
+	// Initialise the timeHandler in exactly the same way we would any normal
+	// struct.
+	th := timeHandler{format: time.RFC1123}
+
+	//Refactoring from function above (eg. timeHandler -> timeHandlerRefactored)
+	th_refactored := http.HandlerFunc(timeHandlerRefactored)
+
+	//Refactored from function above to utlize max.HandleFunc() method
+
+	mux.HandleFunc("/anothertimerefactor", timeHandlerRefactored)
+
+	// Use the http.RedirectHandler() function to create a handler which 307
+	// redirects all requests it receives to http://example.org.
+	rh := http.RedirectHandler("http://example.org", 307)
+
+	// Next we use the mux.Handle() function to register this with our new
+	// servemux, so it acts as the handler for all incoming requests with the URL
+	// path /foo.
+	mux.Handle("/foo", rh)
+
+	mux.Handle("/time", th)
+
+	mux.Handle("/timerefactored", th_refactored)
+
+	log.Print("Listening...")
+
+	// Then we create a new server and start listening for incoming requests
+	// with the http.ListenAndServe() function, passing in our servemux for it to
+	// match requests against as the second parameter.
+	http.ListenAndServe(":3000", mux)
+}
